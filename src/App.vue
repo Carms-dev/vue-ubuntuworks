7b8b1e483a12d254383d4f9f3240dabcf743dca0
Carms Ng <contact@carms-ng.com>
2022-05-28 23:49:21 -0700

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <va-navbar>
+    <template #left>
+      <va-navbar-item>Left</va-navbar-item>
+    </template>
+    <template #center>
+      <va-navbar-item>Center</va-navbar-item>
+    </template>
+    <template #right>
+      <va-navbar-item>Right</va-navbar-item>
+    </template>
+  </va-navbar>
+  <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
+  <!-- <HelloWorld msg="Hello Vue 3 + Vite" /> -->
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,17 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import { 
+  createVuesticEssential, VaNavbar 
+} from 'vuestic-ui'
+import 'vuestic-ui/dist/styles/essential.css'
+import 'vuestic-ui/dist/styles/grid/grid.scss'
+import 'vuestic-ui/dist/styles/global/normalize.scss'
+import 'vuestic-ui/dist/styles/global/typography.scss'
+
+createApp(App)
+  .use(createVuesticEssential({
+    components: { VaNavbar },
+    // plugins: { VaDropdownPlugin },
+    // config: { ... }
+  }))
+  .mount('#app')
