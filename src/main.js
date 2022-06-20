5678d4296fbab354cad85483c7551ed28e958102
Chris Lackey <clackey@runbox.com>
2022-06-20 17:15:28 -0400

    added template header and fixed index.css import

--- src/main.js
+++ src/main.js
@@ -1,22 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import {
-  createVuesticEssential,
-  VaButton,
-} from 'vuestic-ui'
-import 'vuestic-ui/dist/vuestic-ui.css' // out of the box styles
-import 'vuestic-ui/dist/styles/essential.css'
-import 'vuestic-ui/dist/styles/grid/grid.scss'
-import 'vuestic-ui/dist/styles/global/normalize.scss'
-import 'vuestic-ui/dist/styles/global/typography.scss'
-import './styles/index.scss'
+import './styles/index.css'
 
-createApp(App)
-  .use(createVuesticEssential({
-    components: {
-      VaButton,
-    },
-    // plugins: { VaDropdownPlugin },
-    // config: { ... }
-  }))
-  .mount('#app')
+createApp(App).mount('#app')
