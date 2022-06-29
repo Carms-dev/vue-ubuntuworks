c8d9ac119a3c5756a71d46e9534cd41ebf4e086c
Chris Lackey <clackey@runbox.com>
2022-06-29 14:58:51 -0400

    trying out bulma instead...

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,12 +1,3 @@
 <template>
-	<header class="p-2">
-		<div class="container flex justify-between h-24 mx-auto lg:max-w-screen-lg xl:max-w-6xl">
-			<a rel="noopener noreferrer" href="#" aria-label="Back to homepage" class="flex items-center p-2">
-				<img src="../assets/logo.png" class="h-20" alt="">
-			</a>
-			<div class="items-center flex-shrink-0 hidden lg:flex">
-				<button class="self-center px-8 py-3 font-semibold rounded bg-violet-400 text-gray-900">Get Started</button>
-			</div>
-		</div>
-	</header>
+
 </template>
