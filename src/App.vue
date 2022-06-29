22db54d43c1a81bb73f8133a3b4cacd774a11941
Chris Lackey <clackey@runbox.com>
2022-06-29 14:23:05 -0400

    installed spectre.css and fixed gulp config

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
-import Header from './components/Header.vue'
+import HelloWorld from './components/HelloWorld.vue';
+import Header from './components/Header.vue';
 </script>
 
 <template>
